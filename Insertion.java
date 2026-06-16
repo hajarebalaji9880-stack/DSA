502bc4b5618a9d779264aa911067052fffc3e721
Balaji <hajarebalaji9880@gmail.com>
2026-06-16 12:19:07 +0530

    "Add Insertion.java"

---
+++ Insertion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Insertion {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int i=5;
+        int key=18;
+    }
+}
