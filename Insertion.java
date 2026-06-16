0a3330028f32b53dc103d18a6e1015b165723ff4
Balaji <hajarebalaji9880@gmail.com>
2026-06-17 01:38:09 +0530

    DSA 15 days

--- Insertion.java
+++ Insertion.java
@@ -1,12 +1,30 @@
 import java.util.Scanner;
+
 public class Insertion {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[10];
-        for(int i=0;i<arr.length;i++){
+
+        int[] arr = new int[10];
+        
+        for (int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
-        int i=5;
-        int key=18;
+
+        int n = 5;
+        int key = 18;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            arr[i + 1] = arr[i];
+        }
+
+        arr[2] = key;
+
+        n++;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
 }
