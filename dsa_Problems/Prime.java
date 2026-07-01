bde3150584b861d69605af11b3f3d4d6d09288b0
Your Name <your_email@example.com>
2026-07-01 16:48:16 +0530

    1 to 100 prime number

---
+++ dsa_Problems/Prime.java
@@ -0,0 +1,23 @@
+package dsa_Problems;
+
+public class Prime {
+    public static boolean isPrime(int n){
+        if(n<=1)
+            return false;
+
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0)
+                return false;
+
+
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        for(int i=2;i<=100;i++){
+            if(isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
